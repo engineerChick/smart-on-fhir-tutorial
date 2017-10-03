64d9a62672fb339770a4687accc7802ebff7f441
EngineerChick <lisa.butler@krminc.com>
2017-10-03 14:42:30 -0400

    try again

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -145,16 +145,16 @@
   window.drawVisualization = function(p) {
     $('#holder').show();
     $('#loading').hide();
-    $('#fname').html(p.fname);
-    $('#lname').html(p.lname);
-    $('#gender').html(p.gender);
-    $('#birthdate').html(p.birthdate);
-    $('#age').html(p.age);
-    $('#height').html(p.height);
-    $('#systolicbp').html(p.systolicbp);
-    $('#diastolicbp').html(p.diastolicbp);
-    $('#ldl').html(p.ldl);
-    $('#hdl').html(p.hdl);
+    // $('#fname').html(p.fname);
+    // $('#lname').html(p.lname);
+    // $('#gender').html(p.gender);
+     $('#birthdate').html(p.birthdate);
+    // $('#age').html(p.age);
+    // $('#height').html(p.height);
+    // $('#systolicbp').html(p.systolicbp);
+    // $('#diastolicbp').html(p.diastolicbp);
+    // $('#ldl').html(p.ldl);
+    // $('#hdl').html(p.hdl);
   };
 
 })(window);
