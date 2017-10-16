1a021901244199ee6fbabe022e03022c2061e78f
EngineerChick <lisa.butler@krminc.com>
2017-10-16 12:37:37 -0400

    add in Chris M vue changes

--- example-smart-app/app.js
+++ example-smart-app/app.js
@@ -4,30 +4,25 @@ var app = new Vue({
     el: '#app',
     data: {
         show: true,
-        street: '555 Cave Road',
-        city: 'Charles Town',
-        state: 'WV',
-        zipCode: '25414',
-        district: 'Jefferson',
-        country: 'USA',
-        home: '304-261-5555',
-        mobile: '304-489-5555',
-        work: '304-876-5555',
-        answer: 'amazon',
-        days: '5',
-        answer: 'yes',
-        swelling: 'no',
-        glucose: 90,
-        systolic: 120,
-        diastolic: 80,
-        steps: '5500 steps',
-        weight: '175 pounds',
-        timeStamp: '5:30',
-        items: []
+        user: {
+            street: '555 Cave Road',
+            city: 'Charles Town',
+            state: 'WV',
+            zipCode: '25414',
+            district: 'Jefferson',
+            country: 'USA',
+            home: '304-261-5555',
+            mobile: '304-489-5555',
+            work: '304-876-5555'
+        },
+        surveyResponses: [],
+        surveyLoading: true
     },
 
     methods: {
         updateDemographics: function() {
+            var vueInstance = this;
+
             var body = {
                 sessionId: "amzn1.ask.account.AFB7OWNRAC2GATNPXS2O2D7P2Y3SYHWXOZ3RITYBXTIMKBP5E4Y3KXYMJOZ55G3LAT5BCZBYFGCA7DMAR2NQMM7NHIK2IT43N6NRXF36LPO3IATTLIQHYT5TPEO3VQ6BIN4Z7SIGBJ57247QTZGG2Y7YOP4ITT2LEVCEI6CF4J6SSQM3V3KE63464OQLBC2WOWDLM2SKMYZR2EQ",
                 sessionTableName: "KRM_Survey",
@@ -35,10 +30,13 @@ var app = new Vue({
             };
             request('POST', 'https://cors-anywhere.herokuapp.com/https://ne5q2c9xz1.execute-api.us-east-1.amazonaws.com/Prod/usersurveyinfo', {
                 json: body
-            }).getBody('utf8').then(JSON.parse).done(function(res) {
-                console.log(res);
+            }).getBody('utf8').then(JSON.parse).catch(function (err) {
+                console.log(err);
+                vueInstance.surveyLoading = false;
+            }).done(function (res) {
                 var unique = _.uniqBy(res.Items, function(o) {
                     return o.questionKey.S;
+
                 });
 
 
@@ -47,30 +45,21 @@ var app = new Vue({
                     rowObj.question = _.replace(_.get(row, 'question.S'), /['"]+/g, '');
                     rowObj.questionKey = _.replace(_.get(row, 'questionKey.S'), /['"]+/g, '');
                     rowObj.answer = _.replace(_.get(row, 'response.S'), /['"]+/g, '');
-                    var tmp = moment.unix(_.get(row, 'createdOn.S')/1000);
-                    var day = tmp.format('MM-DD-YYYY' + ' ' + 'HH:mm');
-                    console.log(day);
-                    // if ((day.substr(day.length - 5, 2)) < 12) {
-                    //     day += 'am';
-                    // } else {
-                    //     day += 'pm';
-                    // }
+                    var tmp = moment.unix(_.get(row, 'createdOn.S') / 1000);
+                    var day = tmp.format('MM-DD-YYYY hh:mm a');
 
-                    rowObj.lastDay = moment(day).fromNow();
+                    rowObj.lastDay = tmp.fromNow();
 
                     rowObj.date = day;
-                    console.log(day);
-                    console.log('row answer' + rowObj.answer);
 
-                    if(rowObj.questionKey !== 'StartSurvey') {
-                        app.items.push(rowObj);
+                    if (rowObj.questionKey !== 'StartSurvey') {
+                        vueInstance.surveyResponses.push(rowObj);
                     }
 
                 });
-                console.log(app.items);
-            });
-        }
-
+            vueInstance.surveyLoading = false;
+        });
+    }
     }
 });
 app.updateDemographics();
