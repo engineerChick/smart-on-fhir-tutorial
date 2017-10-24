65a121c0a68ae05e1c2482991c8ee88262c0ff8b
EngineerChick <lisa.butler@krminc.com>
2017-10-24 10:38:16 -0400

    *

--- example-smart-app/app.js
+++ example-smart-app/app.js
@@ -142,10 +142,10 @@ var app = new Vue({
                 (results[vital] = results[vital] || []).push(_.filter(res.entry, function(o)
                 { return _.includes(_.get(o, 'resource.id', null), vital); }));
 
-                vueInstance.ohcVitals = _.sortBy(results, [ function(o) {
+                vueInstance.ohcVitals.push( _.sortBy(results, [ function(o) {
                     return new moment(o.effectiveDateTime).format('YYYY-MM-DD');
                     }]
-                );
+                ););
                 vueInstance.surveyLoading = false;
             });
             console.log(vueInstance.ohcVitlas);
